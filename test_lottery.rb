3a4bf822444d9d7395f67707fe8c9e9427c2acd9
stephinwv <thekingdomseeker@aol.com>
2017-07-12 11:36:37 -0400

    Add files via upload
    added additional test paramaters set

--- test_lottery.rb
+++ test_lottery.rb
@@ -6,4 +6,15 @@ class Winning_numbers_test < Minitest::Test
 	def test_1_equals_1
 		assert_equal(1,1)
 	end
+	 def test_for_empty_array
+        ticket = []
+        winner = []
+        assert_equal([],lottery(ticket,winner))
+    end
+
+   def test_returns_winner_array_empty
+        ticket = "1243"
+        winner = []
+        assert_equal([],lottery(ticket,winner))
+    end
 end
